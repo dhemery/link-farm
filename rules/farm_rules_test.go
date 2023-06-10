3954461d999f6923e131b01891b85c52bb40819c
Dale Emery <dale@dhemery.com>
2023-06-10 15:39:14 -0700

    Fix bad assertion in farm path test

--- rules/farm_rules_test.go
+++ rules/farm_rules_test.go
@@ -21,31 +21,31 @@ var farmRuleTests = map[string]farmRuleTest{
 		Path: "path/to/dir-with-farm-file",
 		Want: nil,
 	},
-	"path to dir with no .farm file is error": {
+	"path to dir with no .farm file is invalid": {
 		FS: fstest.MapFS{
 			"path/to/dir-with-no-farm-file": directory(0755),
 		},
 		Path: "path/to/dir-with-no-farm-file",
 		Want: fs.ErrInvalid,
 	},
-	"path to file is error": {
+	"path to file is invalid": {
 		FS: fstest.MapFS{
 			"path/to/file": regularFile(),
 		},
 		Path: "path/to/file",
 		Want: fs.ErrInvalid,
 	},
-	"path to link is error": {
+	"path to link is invalid": {
 		FS: fstest.MapFS{
 			"path/to/link": linkTo("some/place"),
 		},
 		Path: "path/to/link",
 		Want: fs.ErrInvalid,
 	},
-	"path to non-existent entry is error": {
+	"path to non-existent entry is invalid": {
 		FS:   fstest.MapFS{},
 		Path: "path/to/nothing",
-		Want: fs.ErrNotExist,
+		Want: fs.ErrInvalid,
 	},
 }
 
